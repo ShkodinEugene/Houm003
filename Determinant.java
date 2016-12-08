0eb1ae6da65a405eb7bf9f87dfefe68c84998c45
Shkodin Eugene <johnnidotru@rambler.ru>
2016-12-08 16:54:00 +0300

    init commit

--- Determinant.java
+++ Determinant.java
@@ -1,6 +1,6 @@
 
 public class Determinant {
-	 //рекурсивная функция - вычисляет значение определителя. Если на входе определитель 2х2 - просто вычисляем (крест-на-крест), иначе раскладываем на миноры. Для каждого минора вычисляем ЕГО определитель, рекурсивно вызывая ту же функцию..
+	
     public double CalculateMatrix(double[][] matrix){
         double calcResult=0.0;
         if (matrix.length==2){
@@ -9,22 +9,22 @@ public class Determinant {
         else{
             int koeff=1;
             for(int i=0; i<matrix.length; i++){
-                if(i%2==1){  //я решил не возводить в степень, а просто поставить условие - это быстрее. Т.к. я раскладываю всегда по первой (читай - "нулевой") строке, то фактически я проверяю на четность значение i+0.
+                if(i%2==1){  
                     koeff=-1;
                 }
                 else{
                     koeff=1;
                 };
-      //собственно разложение:                
+                    
                 calcResult += koeff*matrix[0][i] * this.CalculateMatrix(this.GetMinor(matrix,0,i)); 
             }
         }
         
-    //возвращаем ответ
+   
         return calcResult;
     }
     
-    //функция, к-я возвращает нужный нам минор. На входе - определитель, из к-го надо достать минор и номера строк-столбцов, к-е надо вычеркнуть.
+   
     private double[][] GetMinor(double[][] matrix, int row, int column){
         int minorLength = matrix.length-1;
         double[][] minor = new double[minorLength][minorLength];
